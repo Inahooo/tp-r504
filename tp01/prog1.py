5e8a8f37c4582f55c2ea5eecb069394ddba89e10
user <arnodjenadi@gmail.com>
2024-09-09 17:15:02 +0200

    TypeError added + two programs working together

--- tp01/prog1.py
+++ tp01/prog1.py
@@ -1,4 +1,11 @@
-print ( " Hello , World ! " )
+import fonctions as f
 
-a= (int(input="Donner un chiffre : "))
-b=
+#print ( " Hello , World ! " )
+
+a=input("Donnez le nombre : ")
+b=input("Donnez son exposant : ")
+res = f.puissance(a,b)
+
+#while True:
+	#a =int(input("Donner un chiffre pour le mettre au carré: "))
+	#print( "Le resultat est : ",pow(a,2))
