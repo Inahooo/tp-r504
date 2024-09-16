a867e868d43c5feba2cda515371563314be41b25
djenaarn <arnodjenadi@gmail.com>
2024-09-16 14:51:21 +0200

    maj

--- tp01/fonctions.py
+++
@@ -1,6 +0,0 @@
-def puissance (a,b) :
-	if not type(a) is int:
-		raise TypeError("Only integers are allowed")
-	if not type(b) is int:
-		raise TypeError("Only integers are allowed")
-	print( "Résultat de l'opération a^b : ",(a**b))
