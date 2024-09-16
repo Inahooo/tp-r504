a40edbaec4dd7a4d6b4bd8b8b83bc0583fda88f5
djenaarn <arnodjenadi@gmail.com>
2024-09-16 16:28:28 +0200

    ajout test limite

--- TP01/test.py
+++ TP01/test.py
@@ -9,6 +9,6 @@ def test_2 ( ):
 	assert f.puissance(-8,2)==64
 def test_3 ( ):
 	assert f.puissance(-2,3)==-8
-	assert f.puissance(-2,0)==-1
+	assert f.puissance(-2,0)==1
 	assert f.puissance(2,0)==1
 	assert f.puissance(-2,-2)==0.25
