253218f004ac50da6724500aa391932098e4dd48
djenaarn <arnodjenadi@gmail.com>
2024-09-16 16:45:29 +0200

    Changement sur le code fonctions

--- TP01/fonctions.py
+++ TP01/fonctions.py
@@ -1,6 +1,17 @@
-def puissance (a,b) :
-	if not type(a) is int:
-		raise TypeError("Only integers are allowed")
-	if not type(b) is int:
-		raise TypeError("Only integers are allowed")
-	return a**b
+
+##def puissance (a,b) :
+##	if not type(a) is int:
+##		raise TypeError("Only integers are allowed")
+##	if not type(b) is int:
+##		raise TypeError("Only integers are allowed")
+##	return a**b
+
+def puissance (a,b):
+	if b==0:
+		return 1
+	if b >0:
+		for i in range(1,b):
+			a= a*b
+		return a
+	if b<0:
+	return 1/puissance (a,-b)
