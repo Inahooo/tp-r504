e6a436480d5a5f7b2baacc093d3e0bbc7e60655f
djenaarn <arnodjenadi@gmail.com>
2024-09-16 16:57:48 +0200

    Changement sur le code fonctions

--- TP01/fonctions.py
+++ TP01/fonctions.py
@@ -9,9 +9,10 @@
 def puissance (a,b) :
 	if b==0:
 		return 1
-	if b >0:
-		for i in range(1,b):
-			a= a*b
-		return a
+	elif b >0:
+		resultat = a
+		for _ in range(1,b):
+			resultat *= a
+		return resultat
 	else:
 		return 1/puissance (a,-b)
