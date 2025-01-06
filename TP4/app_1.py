0632e508e6fe93501600d7541148f9bcfcb0600e
adjenadi <arnodjenadi@gmail.com>
2025-01-06 16:30:14 +0100

    Fin du TP4 + Fin du TP Regex

--- TP4/app_1.py
+++ TP4/app_1.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
+import re
 
 app = Flask(__name__)
  
@@ -13,6 +14,38 @@ db_config = {
 }
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor() 
+
+def validate_username(username):
+    patterns = [
+        (r'.{6,}', "L'identifiant doit avoir au moins 6 caractères."),
+        (r'.*\d.*', "L'identifiant doit contenir au moins un chiffre."),
+        (r'.*[a-z].*', "L'identifiant doit contenir au moins une lettre minuscule."),
+        (r'.*[A-Z].*', "L'identifiant doit contenir au moins une lettre majuscule."),
+        (r'.*[%#{}@].*', "L'identifiant doit contenir au moins un des caractères suivants : #%{}@."),
+    ]
+    
+    # Validation avec les regex
+    for pattern, message in patterns:
+        if re.fullmatch(pattern, username) is None:
+            return False, message
+    
+    return True, "L'identifiant est valide."
+
+@app.route('/newuser/', methods=['GET', 'POST'])
+def new_user():
+    error_message = None
+    success_message = None
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        is_valid, message = validate_username(username)
+        
+        if is_valid:
+            success_message = message
+        else:
+            error_message = message
+    
+    return render_template('newuser.html', error_message=error_message, success_message=success_message)
 
 @app.route('/')
 def index():
